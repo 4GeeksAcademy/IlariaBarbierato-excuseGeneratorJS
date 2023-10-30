6474a41ff2d69ce24b2f5795fbecef54567f46f0
Ilaria Barbierato <145076192+IlariaBa@users.noreply.github.com>
2023-10-30 23:00:25 +0000

    commit Coding Guidelines Challenge

--- src/app.js
+++ src/app.js
@@ -5,28 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog ", "My grandma ", "His turtle ", "My bird "];
-let action = ["ate ", "peed ", "crushed ", "broke "];
-let what = ["my homework ", "the keys ", "the car "];
-let when = [
-  "before the class ",
-  "right on time ",
-  "when I finished ",
-  "during my lunch ",
-  "while I was praying "
-];
-
 function createExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
   var randomIndexWho = Math.floor(Math.random() * who.length);
   var randomIndexAction = Math.floor(Math.random() * action.length);
   var randomIndexWhat = Math.floor(Math.random() * what.length);
   var randomIndexWhen = Math.floor(Math.random() * when.length);
 
-  document.getElementById("excuse").innerHTML =
-    who[randomIndexWho] +
-    action[randomIndexAction] +
-    what[randomIndexWhat] +
-    when[randomIndexWhen];
+  document.getElementById(
+    "excuse"
+  ).innerHTML = `${who[randomIndexWho]} ${action[randomIndexAction]} 
+  ${what[randomIndexWhat]} ${when[randomIndexWhen]}`;
 }
 
 window.onload = createExcuse();
